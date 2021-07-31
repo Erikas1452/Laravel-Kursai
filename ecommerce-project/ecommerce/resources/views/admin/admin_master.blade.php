57125de793fc74d7780a158965eb98edd9d8e292
Erikas1452 <erikastumanovas@gmail.com>
2021-07-31 18:15:41 +0300

    Brands CRUP in admin view

--- ecommerce-project/ecommerce/resources/views/admin/admin_master.blade.php
+++ ecommerce-project/ecommerce/resources/views/admin/admin_master.blade.php
@@ -64,6 +64,8 @@
 	
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
   <script>
     @if(Session::has('message'))
     var type = "{{ Session::get('alert-type','info') }}"
@@ -86,6 +88,35 @@
     }
     @endif 
    </script>
+
+   <script>
+   $(function(){
+    $(document).on('click','#delete',function(e){
+        e.preventDefault();
+        var link = $(this).attr("href");
+        Swal.fire({
+          title: 'Are you sure?',
+          text: "Delete This Data?",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = link
+            Swal.fire(
+              'Deleted!',
+              'Your file has been deleted.',
+              'success'
+            )
+          }
+        }) 
+    });
+
+  });
+  </script>
+
    
 </body>
 </html>
