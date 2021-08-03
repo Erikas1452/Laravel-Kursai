f77a309c00ded2e0019737f69c40d57da016054b
Erikas1452 <erikastumanovas@gmail.com>
2021-08-03 15:31:59 +0300

    Sub Sub categories in sidebar display in frontend

--- ecommerce-project/ecommerce/resources/views/frontend/index.blade.php
+++ ecommerce-project/ecommerce/resources/views/frontend/index.blade.php
@@ -18,26 +18,41 @@
 
               @foreach($categories as $category)
               <li class="dropdown menu-item"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon {{ $category->category_icon }}" aria-hidden="true"></i>
-@if(session()->get('language') == 'hindi') {{ $category->category_name_hin }} @else {{ $category->category_name_en }} @endif
+              @if(session()->get('language') == 'hindi') {{ $category->category_name_hin }} @else {{ $category->category_name_en }} @endif
                 </a>
                 <ul class="dropdown-menu mega-menu">
                   <li class="yamm-content">
                     <div class="row">
 
+                      @php
+                      $subcategories = App\Models\SubCategory::where('category_id',$category->id)->orderBy('subcategory_name_en','ASC')->get();
+                      @endphp                   
+
+                      @foreach($subcategories as $subcategory)
+
                       <div class="col-sm-12 col-md-3">
+
+                        <h2 class="title">
+                        @if(session()->get('language') == 'hindi') {{ $subcategory->subcategory_name_hin }} @else {{ $subcategory->subcategory_name_en }} @endif
+                        </h2>
+
+                        @php
+                        $subsubcategories = App\Models\SubSubCategory::where('subcategory_id',$subcategory->id)->orderBy('subsubcategory_name_en','ASC')->get();
+                        @endphp                
+
+                        @foreach($subsubcategories as $subsubcategory)      
+
                         <ul class="links list-unstyled">
-                          <li><a href="#">Dresses</a></li>
-                          <li><a href="#">Shoes </a></li>
-                          <li><a href="#">Jackets</a></li>
-                          <li><a href="#">Sunglasses</a></li>
-                          <li><a href="#">Sport Wear</a></li>
-                          <li><a href="#">Blazers</a></li>
-                          <li><a href="#">Shirts</a></li>
-                          <li><a href="#">Shorts</a></li>
+                        <li><a href="#">
+                        @if(session()->get('language') == 'hindi') {{ $subsubcategory->subsubcategory_name_hin }} @else {{ $subsubcategory->subsubcategory_name_en }} @endif
+                        </a></li>
+                       
                         </ul>
+
+                        @endforeach
                       </div>
                       <!-- /.col -->
-                       
+              @endforeach
                     </div>
                     <!-- /.row --> 
                   </li>
