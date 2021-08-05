f0c815e643c484c50b3d9c07d2dfb1d0b2a6888d
Erikas1452 <erikastumanovas@gmail.com>
2021-08-05 12:14:20 +0300

    Hot deals displaying in frontend

--- ecommerce-project/ecommerce/app/Http/Controllers/Frontend/IndexController.php
+++ ecommerce-project/ecommerce/app/Http/Controllers/Frontend/IndexController.php
@@ -19,7 +19,8 @@ class IndexController extends Controller
     	$categories = Category::orderBy('category_name_en','ASC')->get();
 		$products = Product::where('status',1)->orderBy('id','DESC')->limit(6)->get();
 		$featured = Product::where('featured',1)->orderBy('id','DESC')->limit(6)->get();
-    	return view('frontend.index',compact('categories','sliders','products','featured'));
+		$hot_deals = Product::where('hot_deals',1)->orderBy('id','DESC')->limit(3)->get();
+    	return view('frontend.index',compact('categories','sliders','products','featured','hot_deals'));
     }
 
     public function UserLogout(){
