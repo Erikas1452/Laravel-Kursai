753034409a64a3741026f3817117e550b9ca2494
Erikas1452 <erikastumanovas@gmail.com>
2021-08-05 15:35:13 +0300

    Added Hot deals and Related Products to Product page

--- ecommerce-project/ecommerce/app/Http/Controllers/Frontend/IndexController.php
+++ ecommerce-project/ecommerce/app/Http/Controllers/Frontend/IndexController.php
@@ -119,7 +119,10 @@ class IndexController extends Controller
 		$size_hin = $product->product_size_hin;
 		$product_size_hin = explode(',', $size_hin);
 
-		return view('frontend.product.product_details',compact('product','multiImag','product_color_en','product_color_hin','product_size_en','product_size_hin'));
+		$cat_id = $product->category_id;
+		$relatedProduct = Product::where('category_id',$cat_id)->where('id','!=',$id)->orderBy('id','DESC')->get();
+
+		return view('frontend.product.product_details',compact('product','multiImag','product_color_en','product_color_hin','product_size_en','product_size_hin','relatedProduct'));
 	}
 
 	public function TagWiseProduct($tag){
