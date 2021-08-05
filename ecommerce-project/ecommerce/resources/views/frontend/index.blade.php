29a92e17da70ddc070fcf6b39eb3d3bdc131961d
Erikas1452 <erikastumanovas@gmail.com>
2021-08-05 11:16:34 +0300

    Product Details in frontend Displaying data

--- ecommerce-project/ecommerce/resources/views/frontend/index.blade.php
+++ ecommerce-project/ecommerce/resources/views/frontend/index.blade.php
@@ -1,5 +1,8 @@
 @extends('frontend.main_master')
 @section('content')
+@section('title')
+Home Easy Online Shop
+@endsection
 
     <div class="body-content outer-top-xs" id="top-banner-and-menu">
         <div class="container">
