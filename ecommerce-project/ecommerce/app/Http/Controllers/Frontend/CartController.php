38020802ffaaa9ed2190e10e6031ed9150334737
Erikas1452 <erikastumanovas@gmail.com>
2021-08-09 12:04:17 +0300

    Product already on a wishlist check

--- ecommerce-project/ecommerce/app/Http/Controllers/Frontend/CartController.php
+++ ecommerce-project/ecommerce/app/Http/Controllers/Frontend/CartController.php
@@ -68,12 +68,19 @@ class CartController extends Controller
     public function AddToWishlist(Request $request, $product_id){
         if (Auth::check()) {
             $exists = Wishlist::where('user_id',Auth::id())->where('product_id',$product_id)->first();
+
+			if(!$exists){
             Wishlist::insert([
                 'user_id' => Auth::id(), 
                 'product_id' => $product_id, 
                 'created_at' => Carbon::now(), 
             ]);
-           return response()->json(['success' => 'Successfully Added On Your Wishlist']);
+           		return response()->json(['success' => 'Successfully Added On Your Wishlist']);
+
+			}else{
+				return response()->json(['error' => 'This Product has Already on Your Wishlist']);
+			}
+
         }else{
             return response()->json(['error' => 'At First Login Your Account']);
         }
