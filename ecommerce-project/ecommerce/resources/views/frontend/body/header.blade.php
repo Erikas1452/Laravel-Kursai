cde9c00cefa3bb17dbd6b9238d1439dac84c4dec
Erikas1452 <erikastumanovas@gmail.com>
2021-08-10 10:20:47 +0300

    Added Checkout to Header and Cart

--- ecommerce-project/ecommerce/resources/views/frontend/body/header.blade.php
+++ ecommerce-project/ecommerce/resources/views/frontend/body/header.blade.php
@@ -12,7 +12,7 @@
                             </a></li>
                         <li><a href="{{ route('wishlist') }}"><i class="icon fa fa-heart"></i>Wishlist</a></li>
                         <li><a href="{{ route('mycart') }}"><i class="icon fa fa-shopping-cart"></i>My Cart</a></li>
-                        <li><a href="#"><i class="icon fa fa-check"></i>Checkout</a></li>
+                        <li><a href="{{ route('checkout') }}"><i class="icon fa fa-check"></i>Checkout</a></li>
 
                         <li>
                             @auth
@@ -139,10 +139,8 @@
 
                      
                                 <div class="clearfix cart-total">
-                                    <div class="pull-right"> <span class="text">Sub Total :</span><span
-                                            class='price'>$600.00</span> </div>
                                     <div class="clearfix"></div>
-                                    <a href="checkout.html"
+                                    <a href="{{ route('checkout') }}"
                                         class="btn btn-upper btn-primary btn-block m-t-20">Checkout</a>
                                 </div>
                                 <!-- /.cart-total-->
